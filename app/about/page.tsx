8048567891dbacaa351c554d34bd175eb3d57176
Clupai8o0 <clupaio4@gmail.com>
2026-01-22 23:39:40 +1100

    smooth scrolling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,18 +6,21 @@ import StoryTimeline from '@/components/about/StoryTimeline';
 import VisionSection from '@/components/about/VisionSection';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import SmoothScroll from '@/components/SmoothScroll';
 
 export default function AboutPage() {
   return (
-    <main className="min-h-screen bg-background">
-      <Navbar />
-      <AboutIntro />
-      <StoryTimeline />
-      <VisionSection />
-      <ExecTeamSection />
-      <CultureSection />
-      <AboutCTA />
-      <Footer />
-    </main>
+    <SmoothScroll>
+      <main className="min-h-screen bg-background">
+        <Navbar />
+        <AboutIntro />
+        <StoryTimeline />
+        <VisionSection />
+        <ExecTeamSection />
+        <CultureSection />
+        <AboutCTA />
+        <Footer />
+      </main>
+    </SmoothScroll>
   );
 }
